9b01fec1395dc4415de7b91c29371f6caad5521e
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-19 00:06:07 +0300

    run&kill server

---
+++ bin/server.sh
@@ -0,0 +1,31 @@
+path=src/server/index.js
+pids=$(lsof -t -i:80);
+
+run_production_server() {
+  if [ -z "$pids" ]
+  then
+    echo '**Server is running**'
+    echo
+    (node $path)&
+    open http://localhost
+  else
+    echo "**Server is already running**"
+    open http://localhost
+  fi
+}
+
+
+if [[ $1 == "kill" ]]
+then 
+  if [ -z "$pids" ]
+  then
+    echo "**Server not running**"
+  else
+    kill $pids
+  fi
+elif [[ $1 == 'product' ]]
+then
+  run_production_server
+else 
+  node $path
+fi
