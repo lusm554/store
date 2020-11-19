d5eaab2cc2de189225b98f91691aa92617889bdd
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-19 03:19:11 +0300

    connect to db

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,15 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const path = require('path')
 const app = express()
 require('dotenv').config({ path: path.join(process.cwd(), 'src', 'config', '.env') })
 
+mongoose.connect(process.env.MongoDB, { useUnifiedTopology: true, useNewUrlParser: true })
+  // Handle initial connection errors
+  .catch(console.error)
+
+// Handle errors after initial connection was established
+mongoose.connection.on('error', console.error)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
