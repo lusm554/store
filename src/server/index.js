ae55628e1d7c487fce399b24ca6459f8b463a1a1
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-19 18:30:03 +0300

    user router

--- src/server/index.js
+++ src/server/index.js
@@ -7,12 +7,16 @@ require('dotenv').config({ path: path.join(process.cwd(), 'src', 'config', '.env
 mongoose.connect(process.env.MongoDB, { useUnifiedTopology: true, useNewUrlParser: true })
   // Handle initial connection errors
   .catch(console.error)
-
 // Handle errors after initial connection was established
 mongoose.connection.on('error', console.error)
+
+// Define Routers
+const { userRouter } = require('./routes/userRouter')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/user', userRouter)
 
 app.listen(process.env.PORT)
