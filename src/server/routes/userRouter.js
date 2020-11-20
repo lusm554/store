46849b352d42b5ab8d819472d94906f979e3d13a
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-20 18:53:46 +0300

    user routes

---
+++ src/server/routes/userRouter.js
@@ -0,0 +1,41 @@
+require('../auth/auth')
+const mongoose = require('mongoose')
+const userRouter = require('express').Router()
+const { userController } = require('../controllers/userController')
+
+userRouter.post('/signup', isFieldsExist, async (req, res) => {
+  const { username, password } = req.user
+
+  let user = new userController.User(username, password)
+  try {
+    user = (await user.save()).fullInfo;
+    if (user.error) {
+      throw new Error(user.error)
+    }
+  } catch (err) {
+    if (user.error instanceof mongoose.Error.ValidationError) {
+      res.status(400).send('Validation failed.')
+    } else {
+      res.status(500).send('Error while creating new user.')
+    }
+    return;
+  }
+
+  res.status(200).json(user.user)
+})
+
+userRouter.post('/signin', userController.signin)
+
+function isFieldsExist(req, res, next) {
+  const { password, username } = req.body
+
+  if (!password || !username) {
+    res.status(400).send('BAD REQUEST')
+    return;
+  }
+
+  req.user = { username, password }
+  next()
+}
+
+exports.userRouter = userRouter
