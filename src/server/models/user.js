5caa48fdf07bc4fdee4db034528ac6b712e7bfb8
loveyousomuch <loveyousomuch554@gmail.com>
2020-11-20 18:51:52 +0300

    user model

---
+++ src/server/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+mongoose.set('useCreateIndex', true)
+const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 20,
+      unique: true
+    },
+    password: { type: String, required: true, trim: true }
+  },
+  {
+    timestamps: true
+  }
+)
+
+UserSchema.statics.findByLogin = async function(username, callback) {
+  if (!callback) {
+    return await this.findOne({ username })
+  }
+  
+  let user;
+
+  try {
+    user = await this.findOne({ username })
+  } catch (error) {
+    callback(error)
+  }
+  callback(null, user)
+}
+
+UserSchema.statics.validate = async function(password) {
+  const user = this
+  const match = await bcrypt.compare(password, user.password)
+
+  return match
+}
+
+const UserModel = mongoose.model('User', UserSchema)
+exports.UserModel = UserModel
